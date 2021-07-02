f91efaa1a0e7d94c9585532d70c140c444cb0743
herotelebot <52422404+herotelebot@users.noreply.github.com>
2021-07-02 12:28:12 +0530

    Update env.py

--- env.py
+++ env.py
@@ -2,6 +2,6 @@ import os
  
  
 class EnvData:
-    BOT_TOKEN = "1350303410:AAFNOYwUGf0o_yxkSkaaOnibp1rHPsdQD_c"  # from @botfather
-    API_ID ="1687826" # your api id from my.telegram.org. Sample :- int("123456")
-    API_HASH = "1af0252a7f97e288e2fff6b0adad5b2f" # your api hash from my.telegram.org Sample :- "fayasnoushad123"
+    BOT_TOKEN = "xxxx"  # from @botfather
+    API_ID ="xxxx" # your api id from my.telegram.org. Sample :- int("123456")
+    API_HASH = "xxxxd" # your api hash from my.telegram.org Sample :- "fayasnoushad123"
